850b014732d9297f4bc5af8dc94d7b958eca43aa
deyaNaydenova <deya.naydenova2010@gmail.com>
2025-05-19 16:06:01 +0300

    statistics

--- Anastasia/index.html/menu.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-    menuBtn.addEventListener('click', () => {
-      navLinks.classList.toggle('open');
-      menuBtn.classList.toggle('open');
-    });
